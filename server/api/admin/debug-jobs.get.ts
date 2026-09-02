aa820a422b0da4440bd49f0c703ba66e8f82e5db
AnjahNyOny <110890442+AnjahNyOny@users.noreply.github.com>
2026-09-02 17:08:56 -0400

    feat: add debug endpoint for bullmq jobs

---
+++ server/api/admin/debug-jobs.get.ts
@@ -0,0 +1,16 @@
+import { Queue } from 'bullmq'
+import { getRedisConnection } from '../../utils/redis'
+
+export default defineEventHandler(async (_event) => {
+  const queue = new Queue('audio-separation', { connection: getRedisConnection() })
+
+  const failedJobs = await queue.getFailed(0, 10)
+  const activeJobs = await queue.getActive(0, 10)
+  const waitingJobs = await queue.getWaiting(0, 10)
+
+  return {
+    failed: failedJobs.map((j) => ({ id: j.id, failedReason: j.failedReason, data: j.data })),
+    active: activeJobs.map((j) => ({ id: j.id, data: j.data })),
+    waiting: waitingJobs.map((j) => ({ id: j.id, data: j.data })),
+  }
+})
